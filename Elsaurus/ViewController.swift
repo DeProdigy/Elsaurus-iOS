f4ca3bdd81e3a1da92b487b03c032bb8ea6e4f08
DeProdigy <alex.hint.ah@gmail.com>
2014-11-16 02:34:28 -0500

    Fix auto layout and close the keyboard when done.

--- Elsaurus/ViewController.swift
+++ Elsaurus/ViewController.swift
@@ -7,6 +7,32 @@
 //
 
 import UIKit
+import SystemConfiguration
+
+public class Reachability {
+    
+    class func isConnectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0)).takeRetainedValue()
+        }
+        
+        var flags: SCNetworkReachabilityFlags = 0
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) == 0 {
+            return false
+        }
+        
+        let isReachable = (flags & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        
+        return (isReachable && !needsConnection) ? true : false
+    }
+    
+}
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
@@ -17,6 +43,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.userInputTextField.delegate = self
+        view.tintColor = UIColor.whiteColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,7 +51,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 
+    
     @IBAction func translationSubmit(sender: UIButton) {
         translateTextFieldText()
     }
@@ -34,34 +66,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
-    func translateTextFieldText() {
-        
-        //        "http://192.168.1.134:3000/translation"
-        
+    func showAlert() {
+        let alert = UIAlertView()
+        alert.title = "Yo!"
+        alert.message = "Seems like you ain't got no internet..."
+        alert.addButtonWithTitle("Thank you")
+        alert.show()
+    }
+    
+    func callApi () {
+        // "http://____:3000/translation"
         var request = NSMutableURLRequest(URL: NSURL(string: "https://elsaurus.herokuapp.com/translation")!)
-        
         var session = NSURLSession.sharedSession()
-        
         request.HTTPMethod = "POST"
-        
-        
         var params = ["text":"\(userInputTextField.text)"] as Dictionary
-        
-        
         var err: NSError?
         
         request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
-        
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-//        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
         
         var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
             
             // json = {"response":"Success","msg":"User login successfully."}
-           
+            
             if(err != nil) {
                 
                 println(err!.localizedDescription)
@@ -71,9 +99,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 println("Response: \(response)")
                 
                 var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
                 println("Body: \(strData)\n\n")
                 
                 var err: NSError?
+                
                 var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as NSDictionary
                 
                 var success = json["response"] as? String
@@ -95,21 +125,31 @@ class ViewController: UIViewController, UITextFieldDelegate {
                     
                     self.translatedText.text = responseMsg
                     
-//                  self.loginStatusLB.text=self.responseMsg
+                    // self.loginStatusLB.text=self.responseMsg
                     
                 })
-                
-                
-                
             }
             
         })
         
+        // Hide the keyboard
+        userInputTextField.resignFirstResponder()
+        
         task.resume()
+    }
+    
+    func translateTextFieldText() {
 
-        
-//        translatedText.text = userInputTextField.text
-        
+        if Reachability.isConnectedToNetwork() {
+            
+            callApi()
+            
+        } else {
+            
+            showAlert()
+            
+        }
+
     }
 
 }
