68915b609c153f964cbfbfbe87293edf0369d32a
DeProdigy <alex.hint.ah@gmail.com>
2014-12-01 21:15:44 -0500

    Clean up the structure of the code, dismiss the keyboard when tapped outside of it

--- Elsaurus/ViewController.swift
+++ Elsaurus/ViewController.swift
@@ -11,43 +11,51 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-//    let ipAddress = "http://192.168.1.94:3000/translation"
+    //MARK: 
+    //MARK: Properties
+    //let ipAddress = "http://192.168.1.94:3000/translation"
     let ipAddress = "https://elsaurus.herokuapp.com/translation"
     
     @IBOutlet weak var translatedText: UITextView!
     @IBOutlet weak var userInputTextField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     
+    //MARK:
+    //MARK: View Controller Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.userInputTextField.delegate = self
         view.tintColor = UIColor.whiteColor()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: "didTap:")
+        view.addGestureRecognizer(tapGesture)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK:
+    //MARK: IBAction
+    @IBAction func translationSubmit(sender: UIButton) {
+        translateTextFieldText()
+    }
+    
+    //MARK:
+    //MARK: Helper Methods
+    func translateTextFieldText() {
+        if Reachability.isConnectedToNetwork() {
+            callTransalteTextAPI()
+        } else {
+            showAlert()
+        }
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        translateTextFieldText()
-        return true
+    func didTap(recognizer:UITapGestureRecognizer) {
+        if userInputTextField.isFirstResponder() {
+            userInputTextField.resignFirstResponder()
+        }
     }
-
-    
-    @IBAction func translationSubmit(sender: UIButton) {
-        translateTextFieldText()
-    }
-    
-//
-//    @IBAction func randomSubmit(sender: UIButton) {
-//        getRandomQuote()
-//    }
 
     
     func showAlert() {
@@ -58,90 +66,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
         alert.show()
     }
     
-    func translateTextFieldText() {
-        if Reachability.isConnectedToNetwork() {
-            callTransalteTextAPI()
-        } else {
-            showAlert()
-        }
-    }
+  
     
-    func getRandomQuote() {
-        if Reachability.isConnectedToNetwork() {
-            callRandomQuoteAPI()
-        } else {
-            showAlert()
-        }
-    }
-    
-    func callRandomQuoteAPI() {
-        
-        var request = NSMutableURLRequest(URL: NSURL(string: ipAddress)!)
-        var session = NSURLSession.sharedSession()
-        request.HTTPMethod = "POST"
-        var params = ["random":true] as Dictionary
-        var err: NSError?
-        
-        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            
-            // json = {"response":"Success","msg":"User login successfully."}
-            
-            if(err != nil) {
-                
-                println(err!.localizedDescription)
-                
-            } else {
-                
-                println("Response: \(response)")
-                
-                var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
-                
-                println("Body: \(strData)\n\n")
-                
-                var err: NSError?
-                
-                var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as NSDictionary
-                
-                var success = json["response"] as? String
-                
-                println("Succes: \(success)")
-                
-                
-                if json["response"] as NSString == "Success"
-                    
-                {
-                    
-                    println("Login Successfull")
-                    
-                }
-                
-                let responseMsg = json["translation"] as String
-                
-                dispatch_async(dispatch_get_main_queue(), {
-                    
-                    self.translatedText.text = responseMsg
-                    
-                    // self.loginStatusLB.text=self.responseMsg
-                    
-                })
-            }
-            
-        })
-        
-        // Hide the keyboard
-        userInputTextField.resignFirstResponder()
-        
-        task.resume()
-    }
-
-
+    //MARK:
+    //MARK: Networking
     
     func callTransalteTextAPI() {
-
         var request = NSMutableURLRequest(URL: NSURL(string: ipAddress)!)
         var session = NSURLSession.sharedSession()
         request.HTTPMethod = "POST"
@@ -153,54 +83,29 @@ class ViewController: UIViewController, UITextFieldDelegate {
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         
         var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            
-            // json = {"response":"Success","msg":"User login successfully."}
-            
             if(err != nil) {
-                
                 println(err!.localizedDescription)
-                
             } else {
-                
                 println("Response: \(response)")
-                
                 var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
-                
                 println("Body: \(strData)\n\n")
-                
                 var err: NSError?
-                
                 var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as NSDictionary
-                
                 var success = json["response"] as? String
-                
                 println("Succes: \(success)")
                 
-                
-                if json["response"] as NSString == "Success"
-                    
-                {
-                    
+                if json["response"] as NSString == "Success" {
                     println("Login Successfull")
-                    
                 }
                 
                 let responseMsg = json["translation"] as String
-                
                 dispatch_async(dispatch_get_main_queue(), {
-                    
                     self.translatedText.text = responseMsg
-                    
-                    // self.loginStatusLB.text=self.responseMsg
-                    
                 })
             }
-            
         })
-        
         // Hide the keyboard
         userInputTextField.resignFirstResponder()
-        
         task.resume()
     }
 }
@@ -209,9 +114,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
 //MARK:
 //MARK: UITextFieldDelegate Methods
 extension ViewController: UITextFieldDelegate {
-    
-    
-    
     
     func changeButtonToRandom() {
         submitButton.setTitle("Random Quote", forState: .Normal)
@@ -234,7 +136,6 @@ extension ViewController: UITextFieldDelegate {
         if (textField.text as NSString).length ==  1 && string == "" {
             // Keep button at "Random" mode
             changeButtonToRandom()
-            println("Backspaced to nothing")
         } else {
             changeButtonToTranslate()
         }
@@ -246,14 +147,10 @@ extension ViewController: UITextFieldDelegate {
         changeButtonToRandom()
         return true
     }
-
     
-    /*
-    
-    
-    
-    
-    */
-
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        translateTextFieldText()
+        return true
+    }
     
 }
